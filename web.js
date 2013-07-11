18db95a78ad7bfbfcc0dfc6c8cf6d27b08e2cff4
Reynaldo Reyna <reynaldo.reyna@gmail.com>
2013-07-11 03:27:46 +0000

    Get Info from file and print

--- web.js
+++ web.js
@@ -1,9 +1,24 @@
 var express = require('express');
 
+var fs = require('fs');
+
 var app = express.createServer(express.logger());
 
+fs.readFileSync('index.html', function (err, data) {
+  if (err) throw err;
+  console.log(data);
+});
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    
+
+    fs.readFileSync('index.html', function (err, data) {
+	if (err) throw err;
+	console.log(data);
+    });
+
+    var text =  data.toString('utf-8');
+    response.send(text);
 });
 
 var port = process.env.PORT || 5000;
